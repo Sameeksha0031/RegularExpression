2afbc82d78dd9bd1b9b4dd813129c665a85b3349
Akash <akhvnd@gmail.com>
2022-08-01 22:17:10 +0530

    First User Name

---
+++ ValidateUserName.java
@@ -0,0 +1,17 @@
+package com.regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class ValidateUserName {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		String userName = sc.next();
+		boolean b3 = Pattern.matches("^[A-Z]{1,20}[A-Za-z]{3,20}$", userName);  
+		System.out.println(b3);
+
+	}
+
+}
