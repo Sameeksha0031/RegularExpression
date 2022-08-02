51b341cc433afed04b60e96c9be73f8d02c66ddb
Akash <akhvnd@gmail.com>
2022-08-02 19:07:00 +0530

    Validate Phone No

--- ValidateUserName.java
+++ ValidateUserName.java
@@ -16,13 +16,17 @@ public class ValidateUserName {
 		System.out.println("Enter the Last Name");
 		String LastName = sc.next();
 		boolean b1 = Pattern.matches("^[A-Z]{1}[a-z]{2,20}$", LastName);  
-		System.out.println(b1);*/
+		System.out.println(b1);
 		
 		System.out.println("Enter the email");
 		String email = sc.next();
 		boolean b2 = Pattern.matches("^[a-z](.+)@(\\S+)$", email);  
-		System.out.println(b2);
+		System.out.println(b2);*/
+		
+		System.out.println("Enter the PhoneNumber");
+		String phoneno = sc.nextLine();
+		boolean b3 = Pattern.matches("^([0-9]{2})? ?[0-9]{1}[0-9]{9}$", phoneno);
+		System.out.println(b3);
 
 	}
-
 }
